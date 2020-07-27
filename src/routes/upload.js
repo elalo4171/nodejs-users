2ec0cffbee0598fafa08d52ab949a4503dc7057a
Eduardo Garcia <elalo417@gmail.com>
2020-07-26 23:20:40 -0500

    Subida de imagenes yes

--- src/routes/upload.js
+++ src/routes/upload.js
@@ -6,7 +6,9 @@ app.use(fileUpload());
 
 
 
-app.put('/upload', (req, res) => {
+app.put('/upload/:id', (req, res) => {
+
+    let { id } = req.params;
     //validar que venga la foto
     if (!req.files) {
         return res.status(400)
@@ -18,11 +20,30 @@ app.put('/upload', (req, res) => {
             });
     }
     //extenciones permitidas
-    const ext = ['png', 'jpg', 'jpeg'];
+    let ext = ['png', 'jpg', 'jpeg', 'JPG'];
 
 
-    let sampleFile = req.files.archivo;
-    sampleFile.mv('uploads/user-foto/iamgen.jpg', function(err) {
+    let archivo = req.files.archivo;
+    let nombreCortado = archivo.name.split('.');
+
+    let extension = nombreCortado[nombreCortado.length - 1];
+    console.log("ID:" + id);
+    if (ext.indexOf(extension) < 0) {
+        return res.status(400).json({
+            ok: false,
+            err: {
+                message: `Las extensiones validad son ${ext.join(' ')}`,
+                ext: extension
+            }
+        })
+    }
+
+    //Cambiar nombre al archivo
+
+    let nombreArchivo = `${id}-${new Date().getMilliseconds()}.${extension}`
+
+    console.log(extension);
+    archivo.mv(`uploads/user-foto/${nombreArchivo}`, function(err) {
         if (err)
             return res.status(500).send(err);
 
